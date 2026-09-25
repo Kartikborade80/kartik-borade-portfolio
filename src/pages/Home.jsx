2cb9380e120b2d2df7f50fe69ccd5f72dfb95b61
KartikBorade <kartikborade8010@gmail.com>
2026-09-25 17:01:44 +0530

    Fix photo framing and 1:1 circle fit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -126,6 +126,7 @@ export default function Home() {
                 width: '100%',
                 height: '100%',
                 objectFit: 'cover',
+                objectPosition: 'center 10%',
                 borderRadius: '50%',
               }}
             />
